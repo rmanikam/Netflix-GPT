5a7b551abfa3f9d355ab756e360b933549897814
rmanikam <sahiraman7@gmail.com>
2024-11-06 21:24:19 +0530

    sign in /sign up form && Login / Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Login from './components/Login';
 
 function App() {
   return (
-    <div className="text-3xl font-bold text-green-800">
-      Namaste Everyone, let's build Netflix GPT
+    <div className="App">
+      <Login />
     </div>
   );
 }
