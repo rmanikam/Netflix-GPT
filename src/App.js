c48fbb564b759f2950908446531fc2224462017e
expertdeveloper5821 <128484693+expertdeveloper5821@users.noreply.github.com>
2024-11-20 17:19:23 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import { Provider } from 'react-redux';
 import './App.css';
 import Body from './components/Body';
-import { Provider } from "react-redux";
 import appStore from "./components/utils/appStore";
 function App() {
   return (
