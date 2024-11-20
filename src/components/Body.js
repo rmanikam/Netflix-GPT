f4dd90e90f8fba5d62c0f709dd3d1c51dc8ca172
expertdeveloper5821 <128484693+expertdeveloper5821@users.noreply.github.com>
2024-11-20 17:53:57 +0530

    changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,30 +1,3 @@
-// import React from 'react'
-// import Login from './Login'
-// import Browse from './Browse'
-// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
-// const Body = () => {
-
-//     const appRouter = createBrowserRouter([
-//         { path: "/", 
-//           element:<Login />
-//         },
-//         { path:"/browse",
-//           element:<Browse />
-//         }
-//     ])
-
-//   return (
-//     <div>
-     
-//      <RouterProvider router ={appRouter}/>
-
-//     </div>
-//   )
-// }
-
-// export default Body;
-
 
 import { createBrowserRouter } from "react-router-dom";
 import Browse from "./Browse";
@@ -42,24 +15,6 @@ const Body = () => {
       element: <Browse />,
     },
   ]);
-
-    useEffect(()=>
-    {
-      onAuthStateChanged(auth, (user) => {
-        // user sign in
-        if (user) {
-         
-          const {uid,email, password,photoURL}= user;
-           dispatch(addUser({uid:uid, email:email, password: password,photoURL:photoURL}));
-          
-        } else {
-           // sign out
-           dispatch(removeUser());
-
-        }
-      })
-
-    },[])
 
   return (
     <div>
